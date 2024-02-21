7b1e31216542671afca9592d968c32e3827df4d7
HW <1979170837@qq.com>
2024-02-21 21:35:31 +0800

    增加flink日志打印

--- bigdata_flink/src/main/java/org/sperri/flink/demo/EventLog.java
+++ bigdata_flink/src/main/java/org/sperri/flink/demo/EventLog.java
@@ -1,15 +1,38 @@
 package org.sperri.flink.demo;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.stream.Stream;
 
 /**
  * @author Jie Zhao
  * @date 2024/2/16 上午11:53
  */
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class EventLog {
 
     private String uid;
+    private Long time;
+    private String pageId;
     private String sip;
     private String host;
+
+    public static EventLog of(String uid, Long time, String pageId) {
+        return new EventLog(uid, time, pageId, null, null);
+    }
+
+    @Override
+    public String toString() {
+        return "EventLog{" +
+                "uid='" + uid + '\'' +
+                ", time=" + time +
+                ", pageId='" + pageId + '\'' +
+                ", sip='" + sip + '\'' +
+                ", host='" + host + '\'' +
+                '}';
+    }
 }
---
+++ bigdata_flink/src/main/java/org/sperri/flink/demo/stream/watermark/WatermarkDemo.java
@@ -0,0 +1,63 @@
+package org.sperri.flink.demo.stream.watermark;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.typeinfo.TypeHint;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.ProcessFunction;
+import org.apache.flink.util.Collector;
+import org.sperri.flink.demo.EventLog;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author Jie Zhao
+ * @date 2024/2/21 20:53
+ */
+@Slf4j
+public class WatermarkDemo {
+
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        DataStreamSource<EventLog> source = env.fromElements(
+                EventLog.of("1", System.currentTimeMillis(), "page01"),
+                EventLog.of("2", System.currentTimeMillis(), "page02")
+        );
+
+        SingleOutputStreamOperator<EventLog> watermarkedStream = source.assignTimestampsAndWatermarks(
+                WatermarkStrategy.<EventLog>forBoundedOutOfOrderness(Duration.of(10, ChronoUnit.SECONDS))
+                        .withTimestampAssigner((event, recordTimestamp) -> event.getTime())
+        );
+
+
+        SingleOutputStreamOperator<Tuple2<String, EventLog>> mappedStream = watermarkedStream
+                .map(event -> new Tuple2<>(event.getUid(), event))
+                .returns(TypeInformation.of(new TypeHint<Tuple2<String, EventLog>>() {
+                    @Override
+                    public TypeInformation<Tuple2<String, EventLog>> getTypeInfo() {
+                        return super.getTypeInfo();
+                    }
+                }));
+
+        SingleOutputStreamOperator<Tuple2<String, EventLog>> processedStream = mappedStream.process(new ProcessFunction<Tuple2<String, EventLog>, Tuple2<String, EventLog>>() {
+            @Override
+            public void processElement(Tuple2<String, EventLog> tuple2, ProcessFunction<Tuple2<String, EventLog>, Tuple2<String, EventLog>>.Context context, Collector<Tuple2<String, EventLog>> collector) throws Exception {
+                long processingTime = context.timerService().currentProcessingTime();
+                long watermark = context.timerService().currentWatermark();
+                System.out.println("当前处理时间：{}" + processingTime);
+                log.info("当前处理时间：{}", processingTime);
+                log.info("当前水位线：{}", watermark);
+                log.info("当前事件：{}", tuple2.f1);
+            }
+        });
+
+        processedStream.print();
+        env.execute();
+    }
+}
